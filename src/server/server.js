45ec865db2ba67928fe7ed8f01e4923ae9f2891f
Andres Canchila <andresfelipecx@gmail.com>
2021-08-28 11:30:03 -0500

    config heroku

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const userRouter = require('../components/user/routes');
 const expenseRouter = require('../components/expense/routes');
 const categoryRouter = require('../components/category/routes');
 const subcategoryRouter = require('../components/subcategory/routes');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/frontend/index.html'));
+});
+
+app.use(express.static(path.join(__dirname, '../../public/frontend')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
